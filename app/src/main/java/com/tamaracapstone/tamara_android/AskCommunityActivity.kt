c7a73490d0379cbd9c81f5d96b6f135f9d2058e3
Andrew Jonatan Damanik <andrewdamanik23@gmail.com>
2023-05-31 10:04:01 +0700

    ask community layout

---
+++ app/src/main/java/com/tamaracapstone/tamara_android/AskCommunityActivity.kt
@@ -0,0 +1,11 @@
+package com.tamaracapstone.tamara_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AskCommunityActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ask_community)
+    }
+}
