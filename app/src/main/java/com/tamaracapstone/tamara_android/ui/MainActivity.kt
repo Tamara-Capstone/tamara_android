aa81ef76be3490371a133889a064936f8497c2f2
Fiona <fionakharisma01@gmail.com>
2023-06-11 18:04:41 +0700

    fragment profile, home, community

--- app/src/main/java/com/tamaracapstone/tamara_android/ui/MainActivity.kt
+++ app/src/main/java/com/tamaracapstone/tamara_android/ui/MainActivity.kt
@@ -2,11 +2,40 @@ package com.tamaracapstone.tamara_android.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.tamaracapstone.tamara_android.R
+import com.tamaracapstone.tamara_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+                R.id.home -> replaceFragment(Home())
+                R.id.community -> replaceFragment(Community())
+                R.id.profile -> replaceFragment(Profile())
+
+                else -> {
+
+                }
+            }
+
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
