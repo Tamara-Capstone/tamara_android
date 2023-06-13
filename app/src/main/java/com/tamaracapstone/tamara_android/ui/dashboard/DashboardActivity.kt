24f1bf343a126cb78d09d96e4b589d08f1fb7df7
Andrew Jonatan Damanik <andrewdamanik23@gmail.com>
2023-06-13 08:34:05 +0700

    update dashboard

--- app/src/main/java/com/tamaracapstone/tamara_android/ui/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/tamaracapstone/tamara_android/ui/dashboard/DashboardActivity.kt
@@ -1,12 +1,29 @@
 package com.tamaracapstone.tamara_android.ui.dashboard
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.tamaracapstone.tamara_android.R
+import com.tamaracapstone.tamara_android.databinding.ActivityDashboardBinding
 
 class DashboardActivity : AppCompatActivity() {
+
+    private var _binding: ActivityDashboardBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        _binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.fragmentContainerView)
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
+
+    fun setBottomNavVisibility(isVisible: Boolean) {
+        binding.bottomNavigationView.visibility = if (isVisible) View.VISIBLE else View.GONE
+    }
+
 }
